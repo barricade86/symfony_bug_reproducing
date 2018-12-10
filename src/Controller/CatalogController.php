cd1300078ba3587699b6a8aeaa370fdce72e8dee
iks_rotarran <iksrotarran.empire@gmail.com>
2018-12-10 23:00:36 +0300

    first commit

---
+++ src/Controller/CatalogController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class CatalogController
+{
+    public function index(string $siteSection, string $categoryPath): Response
+    {
+        die('petaQ');
+    }
+
+    public function halfIndex(): Response
+    {
+        die('petaQ');
+    }
+}
